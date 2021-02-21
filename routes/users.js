020f2a4fc77375ca1b5d2072f7a0f50573d2d15d
sammy <sammy@others>
2021-02-21 12:38:31 +0530

    Commit-4

--- routes/users.js
+++ routes/users.js
@@ -59,7 +59,8 @@ router.post("/forgotPassword", async (req,res)=>{
                 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n'
                 + 'Please click on the following link, or paste this into your browser to complete the process within one hour of receiving it:\n\n'
                 + `https://login-reg-app.herokuapp.com/reset/${token}\n\n`
-                + 'If you did not request this, please ignore this email and your password will remain unchanged.\n',
+                + 'If you did not request this, please ignore this email and your password will remain unchanged.\n'
+                + 'This link will expire in one hour.\n',
             };
 
             await transport.sendMail(mailOptions)
